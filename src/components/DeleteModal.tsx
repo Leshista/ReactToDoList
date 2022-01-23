522861600da64315c97fbab8ed2140fba9cc5d28
leshista <a.leshista@gmail.com>
2022-01-23 18:43:47 +0300

    Made modal handlers nicer

--- src/components/DeleteModal.tsx
+++ src/components/DeleteModal.tsx
@@ -1,13 +1,23 @@
 import './DeleteModal.css'
 
-export default function DeleteModal(props:any) {
+
+export default function DeleteModal(props: any) {
+
+    const confirmHandler = () =>{
+        props.onClick()
+    }
+
+    const cancelHandler = () => {
+        props.onClick()
+    }
     return (
+
         <div className="modal">
             <div className="delete-modal">
                 <p className="delete-modal__title">Are you sure?</p>
                 <div className="delete-modal__buttons">
-                    <button className="delete-modal__cancel" onClick={props.onClick}>Cancel</button>
-                    <button className="delete-modal__confirm" onClick={props.onClick}>Confirm</button>
+                    <button className="delete-modal__cancel" onClick={cancelHandler}>Cancel</button>
+                    <button className="delete-modal__confirm" onClick={confirmHandler}>Confirm</button>
                 </div>
             </div>
         </div>
