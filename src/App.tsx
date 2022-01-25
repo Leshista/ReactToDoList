f7c9bdc836e9dc6121f9fee8b26cbd9083f8e812
leshista <a.leshista@gmail.com>
2022-01-25 10:25:25 +0300

    Deleted all components, will redo everything from scratch

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import React, {FC} from 'react';
-
-import ToDoBlock from './components/ToDoBlock';
+import {FC} from 'react';
 
 const App:FC = () => {
   
   return (
-    <main className='main'>
-      <p className="title">To Do List</p>
-      <ToDoBlock />
-    </main>
+    <div></div>
   );
 }
 
